412bac613b42e8f386d3f629b49d0ebec3ccfd9f
Yacinth <fevriercjjc@gmail.com>
2019-01-10 23:54:05 +0100

    update WTF_pyramid

--- lib/01_pyramids.rb
+++ lib/01_pyramids.rb
@@ -33,6 +33,34 @@ def reverse_pyramid
 end
 
 
+def wtf_pyramid
+    puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ? (choisis un nombre impair)"
+    print "> "
+    user_number = gets.chomp.to_i #to_i récupère un nombre entier à l'entrée (comme ça si on introduit un symbole, le programme plante pas
+    while user_number % 2 == 0 #tant que le nombre est bien pair -> on passe dans cette boucle
+        puts "Même joueur joue encore. Ramsou voudrait un nombre impair. Combien d'étages veux-tu ?"
+        print "> "
+        user_number = gets.chomp.to_i
+    end
+    nb = 1
+    puts "Voici la pyramide :"
+    #le triange supérieur du losange:
+    top = (user_number.to_f / 2).to_i+1 #to_f converti le nombre d'étages en float (nombre décimal) pour permettre une division exacte ; to_i+1 permet d'avoir l'entier supérieur après la division
+    while (nb <= top && nb < 26)
+        nb_space = top - nb
+        puts "#{ " " * nb_space + "#" * nb }" + "#{ "#" * ( nb-1 ) + " " * nb_space }"
+        nb = nb+1
+    end
+    
+    #le triange inferieur du losange:
+    nb = (user_number.to_f / 2).to_i
+    while (nb > 0)
+        nb_space = (user_number.to_f / 2).to_i+1 - nb
+        puts "#{ " " * nb_space + "#" * nb }" + "#{ "#" * ( nb-1 ) + " " * nb_space }"
+        nb = nb-1
+    end
+end
+
 half_pyramid
 full_pyramid
-reverse_pyramid
+wtf_pyramid
