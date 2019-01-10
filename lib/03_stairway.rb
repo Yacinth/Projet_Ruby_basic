9a053364f365033864d670b52b7292e8b239c818
Yacinth <fevriercjjc@gmail.com>
2019-01-10 18:17:14 +0100

    update 2.4.1

--- lib/03_stairway.rb
+++ lib/03_stairway.rb
@@ -1,23 +1,31 @@
 def stairs
 stair = 1
-while (stair <= 10)
+ while (stair <= 10)
     puts ("# " * stair)
-#.rjust(30, '        T                   ') #(a).rjust(b) // espace = b-(nb caractères a) // (nb de caractères a)<b -> il rajoute une chaîne de caractères devant "string a" d'une valeur "b-(nb caractères a)"
+                #.rjust(30, '        T                   ') #(a).rjust(b) // espace = b-(nb caractères a) // (nb de caractères a)<b -> il rajoute une chaîne de caractères devant "string a" d'une valeur "b-(nb caractères a)"
     stair += 1
     
+ end
 end
-    s = 6
-end
+
 
 def guy
     puts "T"
 end
 
-def test(stairs)
-    if stairs == 6
-        puts "T"
+
+def tableau
+ my_array = [ ]
+ my_array << "#{stairs}#{guy}"
+ puts my_array
 end
-    end
+
+def dé
+ puts rand(1...6)
+end
+
+
+tableau
+dé
+  
     
-    
-test(stairs)
