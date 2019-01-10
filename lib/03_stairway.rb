0899e0e59892977f6f6c47ae62dc312b5e5ac826
a1iya <aliyusha.gizatullina@gmail.com>
2019-01-10 16:57:19 +0100

    2.4 stairs

---
+++ lib/03_stairway.rb
@@ -0,0 +1,5 @@
+stairs = 1
+while (stairs <= 10)
+    puts ("# " * stairs).rjust(30) #(a).rjust(b) // espace = b-(nb caractères a) // (nb de caractères a)<b -> il rajoute une chaîne de caractères devant "string a" d'une valeur "b-(nb caractères a)"
+    stairs += 1
+end
